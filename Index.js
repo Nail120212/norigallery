37167ca3ea0c539bc5948395ad371938120c468c
Nail120212 <ianangeloi122@gmail.com>
2026-07-23 20:07:20 +0800

    Refactor routes for tune and sticker endpoints

--- Index.js
+++ Index.js
@@ -6,42 +6,45 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const SGallery = require('./SGallery');
-const Tune = require('./Tune');
-
-app.use('/api/sgallery', SGallery.router);
-app.use('/api/tune', Tune.router);
-
+// Landing
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Tune routes - all serve tune.html (it parses id from path/query)
 app.get('/tune', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'tune.html'));
 });
-
-app.get('/SG', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'sgallery.html'));
-});
-
-app.get('/SG/Id=:pasteId', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'sgallery.html'));
-});
-
-app.get('/SG/:pasteId', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'sgallery.html'));
-});
-
-app.get('/s/:pasteId', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'sgallery.html'));
-});
-
 app.get('/t/:pasteId', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'tune.html'));
 });
+app.get('/tune/:pasteId', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'tune.html'));
+});
+
+// Sticker routes - all serve sgallery.html
+app.get('/SG', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sgallery.html'));
+});
+app.get('/SG/Id=:pasteId', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sgallery.html'));
+});
+app.get('/SG/:pasteId', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sgallery.html'));
+});
+app.get('/s/:pasteId', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sgallery.html'));
+});
+app.get('/sticker', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sgallery.html'));
+});
+app.get('/sticker/:pasteId', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sgallery.html'));
+});
 
 app.listen(PORT, () => {
-    console.log(`[NexxToHubOnTop] Running on http://localhost:${PORT}`);
+    console.log(`[NexxToHubOnTop] Running on http://localhost:${PORT} - 3 files only: index, tune, sgallery`);
 });
 
 module.exports = app;
+
